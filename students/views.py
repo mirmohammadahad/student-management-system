55491f737b575236c42534455c6eb5a8fbafe285
Mir Mohammad Ahad <mdahad626675@gmail.com>
2026-09-05 23:20:19 +0300

    Update student templates and student list

--- students/views.py
+++ students/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Student
 
-# Create your views here.
+
+def student_list(request):
+    students = Student.objects.all()
+
+    return render(request, 'students/student_list.html', {
+        'students': students
+    })
