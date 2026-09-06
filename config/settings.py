d7f9d81c942725c87dcdf3fc73b98a049ec55fe2
Mir Mohammad Ahad <mdahad626675@gmail.com>
2026-09-06 21:10:17 +0300

    Prepare Django project for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-prncf+b6xjyjtnafsl0yr65qp1@9anh%m1td0rgqw2))u-ijx&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Email
