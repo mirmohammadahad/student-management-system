ae4b416a7ec3b30aee71ad18084523b285b00479
Mir Mohammad Ahad <mdahad626675@gmail.com>
2026-09-07 12:15:45 +0300

    Fix allowed hosts for Render

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,11 @@ SECRET_KEY = 'django-insecure-change-this-later'
 # Development mode
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'student-management-system-3-3rk1.onrender.com',
+]
 
 
 # Application definition
